8ea8b10ff20ad61006cfe0160af5f03c8caac411
Audhi Aprilliant <38733334+audhiaprilliant@users.noreply.github.com>
2021-01-04 18:31:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def predict():
 	    return render_template('result.html', prediction = prediction)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host = '0.0.0.0')
